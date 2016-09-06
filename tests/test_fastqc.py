421e23c53ecbcdf22b1048943394705a84a8ce91
Justin Fear <justin.fear@nih.gov>
2016-09-06 14:05:36 -0400

    Working version of fastqc parser

--- tests/test_fastqc.py
+++ tests/test_fastqc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
 from oliverlib import fastqc
 
 BLOCK = """>>Per base sequence quality	pass
@@ -16,6 +19,20 @@ BLOCK = """>>Per base sequence quality	pass
 10	38.47594460091534	39.0	39.0	39.0	37.0	39.0
 """
 
+BLOCK_DF = pd.DataFrame({'Base': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                         'Mean': [33.325882537780295, 33.44737950739895, 33.446659779075645,
+                                  36.67489445866323, 36.658260994356844, 36.65553688182567,
+                                  36.65730974503444, 36.62655009906297, 38.53114508354548,
+                                  38.47594460091534],
+                         'Median': [34.0, 34.0, 34.0, 37.0, 37.0, 37.0, 37.0, 37.0, 39.0, 39.0],
+                         'Lower Quartile': [34.0, 34.0, 34.0, 37.0, 37.0, 37.0, 37.0, 37.0, 39.0, 39.0],
+                         'Upper Quartile': [34.0, 34.0, 34.0, 37.0, 37.0, 37.0, 37.0, 37.0, 39.0, 39.0],
+                         '10th Percentile': [31.0, 31.0, 31.0, 37.0, 35.0, 35.0, 35.0, 35.0, 38.0, 37.0],
+                         '90th Percentile': [34.0, 34.0, 34.0, 37.0, 37.0, 37.0, 37.0, 37.0, 39.0, 39.0]})
+
+BLOCK_DF.set_index('Base', inplace=True)
+
+
 class TestFastqc(unittest.TestCase):
     def setUp(self):
         self.filename = 'tests/data/fastqc_data.txt'
@@ -24,9 +41,16 @@ class TestFastqc(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_FastQCBlock(self)
+    def test_FastQCBlock(self):
         fq = fastqc.FastQCBlock(BLOCK.split('\n'))
-        print(fq.df)
+        assert_frame_equal(fq.df, BLOCK_DF[fq.df.columns])
+        
+    def test_fileParse(self):
+        fq = fastqc.FastQC('test', 'tests/data/fastqc_data.txt')
+        assert_frame_equal(fq['Per base sequence quality'].df, 
+                           BLOCK_DF[fq['Per base sequence quality'].df.columns])
+
+
 
 
 if __name__ == '__main__':
--- tests/test_gtf2IntronBed.py
+++ tests/test_gtf2IntronBed.py
@@ -2,46 +2,46 @@ import os
 import subprocess as sp
 from unittest import TestCase
 
-class TestreadGFF(TestCase):
-    def setUp(self):
-        from urllib import request
-        import gzip
-
-        gff = 'ftp://ftp.flybase.net/releases/FB2016_04/dmel_r6.12/gff/dmel-all-r6.12.gff.gz'
-        self.gffname = '/tmp/dmel-all-r6.12.gff.gz'
-
-        gtf = 'ftp://ftp.flybase.net/releases/FB2016_04/dmel_r6.12/gtf/dmel-all-r6.12.gtf.gz'
-        self.gtfname = '/tmp/dmel-all-r6.12.gtf.gz'
-    
-        # Download URLs unless I already have a copy
-        if not os.path.exists(self.gffname):
-            request.urlretrieve(gff, filename=self.gffname)
-
-        if not os.path.exists(self.gtfname):
-            request.urlretrieve(gtf, filename=self.gtfname)
-
-        # Extract gz's
-        if not os.path.exists(self.gffname.strip('.gz')):
-            proc1 = sp.check_call('gunzip -c {0} > {1}'.format(self.gffname, self.gffname.strip('.gz')), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-
-        if not os.path.exists(self.gtfname.strip('.gz')):
-            proc2 = sp.check_call('gunzip -c {0} > {1}'.format(self.gtfname, self.gtfname.strip('.gz')), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
-
-    def tearDown(self):
-        pass
-        # Delete the extracted files
-        #os.unlink(self.gffname.strip('.gz'))
-        #os.unlink(self.gtfname.strip('.gz'))
-
-        # Delete created databases
-        #os.unlink(self.gffname.strip('.gz') + '.db')
-        #os.unlink(self.gtfname.strip('.gz') + '.db')
-
-    def test_readGFF_zipped_gff(self):
-        from gtf2IntronBed import readGFF
-        readGFF(self.gffname)
-        
-
+# class TestreadGFF(TestCase):
+#     def setUp(self):
+#         from urllib import request
+#         import gzip
+# 
+#         gff = 'ftp://ftp.flybase.net/releases/FB2016_04/dmel_r6.12/gff/dmel-all-r6.12.gff.gz'
+#         self.gffname = '/tmp/dmel-all-r6.12.gff.gz'
+# 
+#         gtf = 'ftp://ftp.flybase.net/releases/FB2016_04/dmel_r6.12/gtf/dmel-all-r6.12.gtf.gz'
+#         self.gtfname = '/tmp/dmel-all-r6.12.gtf.gz'
+#     
+#         # Download URLs unless I already have a copy
+#         if not os.path.exists(self.gffname):
+#             request.urlretrieve(gff, filename=self.gffname)
+# 
+#         if not os.path.exists(self.gtfname):
+#             request.urlretrieve(gtf, filename=self.gtfname)
+# 
+#         # Extract gz's
+#         if not os.path.exists(self.gffname.strip('.gz')):
+#             proc1 = sp.check_call('gunzip -c {0} > {1}'.format(self.gffname, self.gffname.strip('.gz')), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+# 
+#         if not os.path.exists(self.gtfname.strip('.gz')):
+#             proc2 = sp.check_call('gunzip -c {0} > {1}'.format(self.gtfname, self.gtfname.strip('.gz')), shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+# 
+#     def tearDown(self):
+#         pass
+#         # Delete the extracted files
+#         #os.unlink(self.gffname.strip('.gz'))
+#         #os.unlink(self.gtfname.strip('.gz'))
+# 
+#         # Delete created databases
+#         #os.unlink(self.gffname.strip('.gz') + '.db')
+#         #os.unlink(self.gtfname.strip('.gz') + '.db')
+# 
+#     def test_readGFF_zipped_gff(self):
+#         from gtf2IntronBed import readGFF
+#         readGFF(self.gffname)
+#         
+# 
 
 
 
